ca239cfe0e39415a68202ecd46bd290c341a9f18
Pierre J. Callies <35848450+usa85@users.noreply.github.com>
2019-11-07 11:44:26 -0600

    with rogers as center

--- assets/javascript/statisticsdatamaps.js
+++ assets/javascript/statisticsdatamaps.js
@@ -39,7 +39,7 @@
 
   function initMap() {
     // The location of Uluru
-    var rogers = {lat: 45.027728, lng: -93.312869};
+    var rogers = {lat: 45.194911, lng: -93.552053};
     // The map, centered at Uluru
     var map = new google.maps.Map(
         document.getElementById('map'), {zoom: 4, center: rogers});
